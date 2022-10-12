1688c260ea45eee4dcf1ab5fd7b9b29268733f55
juancarlos301 <juancarlos.31.2002@gmail.com>
2022-10-12 12:01:07 -0500

    add api project

---
+++ libs/sequelize.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require("sequelize");
+
+const { config } = require("../config/config");
+const setUpModels = require("../db/models");
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+//const URL = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URL = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+//dialect: "mysql",
+const sequelize = new Sequelize(URL, {
+  dialect: "postgres",
+  logging: true
+});
+
+setUpModels(sequelize);
+module.exports = sequelize;
---
+++ services/order.service.js
@@ -0,0 +1,60 @@
+const boom = require("@hapi/boom");
+
+const { models } = require("../libs/sequelize");
+
+class OrderService {
+  constructor() {}
+
+  async create(data) {
+    const NewOrder = await models.Order.create(data);
+    return NewOrder;
+  }
+
+  async addItem(data) {
+    const NewItem = await models.OrderProduct.create(data);
+    return NewItem;
+  }
+
+  async find() {
+    const orders = await models.Order.findAll({
+      include: [
+        {
+          association: "customer",
+          include: ["user"]
+        }
+      ]
+    });
+    return orders;
+  }
+
+  async findOne(id) {
+    const order = await models.Order.findByPk(id, {
+      include: [
+        {
+          association: "customer",
+          include: ["user"]
+        },
+        "items"
+      ]
+    });
+    if (!order) {
+      throw boom.notFound("order not found");
+    }
+    return order;
+  }
+
+  async update(id, changes) {
+    return {
+      id,
+      changes
+    };
+  }
+
+  async delete(id) {
+    const order = await this.findOne(id);
+    await order.destroy();
+    return { id };
+  }
+}
+
+module.exports = OrderService;
